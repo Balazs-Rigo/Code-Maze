54667cfab6a4ea42ff3b48bbe06e850a5db3e21c
Balazs Rigo <rigo.balazs86@gmail.com>
2022-11-27 19:41:13 +0100

    Chapter 7 - Content Negotiation

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -12,8 +12,7 @@ namespace CompanyEmployees.Presentation.Controllers
 
         [HttpGet]
         public IActionResult GetCompanies()
-        {
-            throw new Exception("Exception");
+        {            
             var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
 
             return Ok(companies);
