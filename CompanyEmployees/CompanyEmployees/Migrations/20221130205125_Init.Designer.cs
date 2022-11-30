7ea2a573ab6f8f565ef90f9f4cf6141f357290d2
Balazs Rigo <rigo.balazs86@gmail.com>
2022-11-30 22:59:33 +0100

    Chapter 9 in progress

--- CompanyEmployees/CompanyEmployees/Migrations/20221130205125_Init.Designer.cs
+++ CompanyEmployees/CompanyEmployees/Migrations/20221130205125_Init.Designer.cs
@@ -12,7 +12,7 @@ using Repository;
 namespace CompanyEmployees.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    [Migration("20221126144932_Init")]
+    [Migration("20221130205125_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
