f0b09327753583b1d197e036b6ece6724f373ff2
Balazs Rigo <rigo.balazs86@gmail.com>
2022-12-03 15:51:37 +0100

    Chapter 18 - SEARCHING

--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Repository.Extensions;
+
 
 namespace Repository
 {
@@ -17,17 +19,20 @@ namespace Repository
         {
         }
 
-        public async Task<PagedList<Employee>> 
-            GetEmployeesAsync(Guid companyId,EmployeeParameters employeeParameters, bool trackChanges)
+        public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
+            EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && 
-            (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
+           trackChanges)
+            .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+            .Search(employeeParameters.SearchTerm)
             .OrderBy(e => e.Name)
             .ToListAsync();
             return PagedList<Employee>
             .ToPagedList(employees, employeeParameters.PageNumber,
            employeeParameters.PageSize);
         }
+
 
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
